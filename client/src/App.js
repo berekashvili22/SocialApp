c0caa33c899546e94512d095f478f38128f8e7fd
tornike <tokoberekashvili22@gmail.com>
2021-07-17 22:09:06 +0400

    SA-2 semanticUI/Routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
    return (
-      <div>
-         <h1>hi tornike</h1>
-      </div>
+      <Router>
+         <Container>
+            <MenuBar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+         </Container>
+      </Router>
    );
 }
 
