74c5678438855475d8725453767d7a3ee39bbd2e
tornike <tokoberekashvili22@gmail.com>
2021-07-25 18:48:02 +0400

    SA-4 implement auth middleware

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,21 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
 
+import { AuthProvider } from './context/auth/auth-context';
+import AuthRoute from './utils/middlewares/auth-middleware';
+
 function App() {
    return (
-      <Router>
-         <Container>
-            <MenuBar />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-         </Container>
-      </Router>
+      <AuthProvider>
+         <Router>
+            <Container>
+               <MenuBar />
+               <Route exact path="/" component={Home} />
+               <AuthRoute exact path="/login" component={Login} />
+               <AuthRoute exact path="/register" component={Register} />
+            </Container>
+         </Router>
+      </AuthProvider>
    );
 }
 
