ce09b3a7fd5b20ba48c71f757cc30235aa2b4317
tornike <tokoberekashvili22@gmail.com>
2021-07-25 18:47:52 +0400

    SA-4 created auth middleware

---
+++ client/src/utils/middlewares/auth-middleware.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { AuthContext } from '../../context/auth/auth-context';
+
+const AuthRoute = ({ component: Component, ...rest }) => {
+   const { user } = useContext(AuthContext);
+
+   return (
+      <Route
+         {...rest}
+         render={(props) => (user ? <Redirect to="/" /> : <Component {...props} />)}
+      />
+   );
+};
+
+export default AuthRoute;
