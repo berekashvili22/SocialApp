371701e1a2b58ae328e24934b6f4e588d7cd49b6
tornike <tokoberekashvili22@gmail.com>
2021-07-26 12:17:22 +0400

    SA-5 automatically pass auth bearer token

--- client/src/Providers/ApolloProvider.js
+++ client/src/Providers/ApolloProvider.js
@@ -4,14 +4,24 @@ import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 const ApolloProviderWrapper = () => {
    const httpLink = createHttpLink({
       uri: 'http://localhost:5000'
    });
 
+   const authLink = setContext(() => {
+      const jwt = localStorage.getItem('JWT');
+      return {
+         headers: {
+            Authorization: jwt ? `Bearer ${jwt}` : ''
+         }
+      };
+   });
+
    const client = new ApolloClient({
-      link: httpLink,
+      link: authLink.concat(httpLink),
       cache: new InMemoryCache()
    });
 
